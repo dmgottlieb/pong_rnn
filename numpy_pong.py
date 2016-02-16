617f7a97f8453d2d69c97e628520e56ca89bd068
David Gottlieb <dmgottlieb@gmail.com>
2016-02-16 13:50:33 -0800

    initial commit

---
+++ numpy_pong.py
@@ -0,0 +1,25 @@
+# small numpy implementation of Pong for programmatic interaction
+
+import numpy as np
+
+class PongState(object):
+
+	
+
+	def __init__():
+		self.screen_buffer = np.zeros((32,32))
+
+
+	def draw():
+		self.screen_buffer = np.zeros((32,32))
+
+		self.draw_scores(self.screen_buffer)
+		self.draw_paddles(self.screen_buffer)
+		self.draw_ball(self.screen_buffer)
+		
+		return self.screen_buffer
+
+	def update():
+		pass
+
+
