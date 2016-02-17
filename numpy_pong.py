a34ed41b0607214875bc99def134f6ac05b7f60c
David Gottlieb <dmgottlieb@gmail.com>
2016-02-16 16:48:34 -0800

    main loop writes images instead of doing live plot

--- numpy_pong.py
+++ numpy_pong.py
@@ -5,6 +5,8 @@
 # small numpy implementation of Pong for programmatic interaction
 #
 # matplotlib is used for live visualization (in __main__)
+#
+# game can be started with a seed to get deterministic outcomes
 
 import numpy as np
 import matplotlib.pyplot as plt
@@ -15,11 +17,12 @@ class PongState(object):
 
 	
 
-	def __init__(self):
+	def __init__(self, seed=None):
+		self.rnd = np.random.RandomState(seed)
 		self.screen_buffer = np.zeros((32,32), dtype=np.uint8)
-		self.paddle_1 = self.Paddle(side='left')
-		self.paddle_2 = self.Paddle(side='right')
-		self.ball = self.Ball()
+		self.paddle_1 = self.Paddle(side='left', rnd=self.rnd)
+		self.paddle_2 = self.Paddle(side='right', rnd=self.rnd)
+		self.ball = self.Ball(rnd=self.rnd)
 		self.scores = None
 
 
@@ -51,20 +54,51 @@ class PongState(object):
 		screen_buffer[top[0]:top[0]+size, top[1]:top[1]+size] = 1
 
 	def update_paddle(self, paddle, input): 
-		pass
+		# detect input
+		# in this model, input is simply {-1, 1} corresponding to moving down or up respectively
+		# 0 is no movement
+
+		if input == None:
+			dm = 0
+		else: 
+			dm = input 
+
+
+		# update momentum
+		paddle.momentum += dm
+
+		# decay / clip momentum
+		paddle.momentum = np.clip(paddle.momentum, -2,2)
+
+		# check for out-of-bounds
+		if (paddle.top[0] < 0 or paddle.top[0] + paddle.size > 31): 
+			pass#paddle.momentum *= -1
+
+		# update position
+		paddle.top[0] += paddle.momentum
+		paddle.top[0] = np.clip(paddle.top[0], 0, 31 - paddle.size)
+
+
 
 	def update_ball(self, ball, paddle_1, paddle_2): 
 		
 		# collision detection
+		# case: paddles
 		for p in [paddle_1, paddle_2]: 
-			p_closest = np.argmin(np.absolute(np.arange(p.top[0], p.top[0]+p.size) - ball.top[0]+ball.size/2))
+			p_closest = np.argmin(np.absolute(np.arange(p.top[0], p.top[0]+p.size) - ball.top[0]+ball.size/2)) + p.top[0]
+			print p_closest
 			d = ball.top - np.asarray((p_closest, p.top[1]))
 			if np.linalg.norm(d) < 2: 
 				ball.momentum[1] *= -1
-				ball.momentum[0] += p.momentum[0]
+				ball.momentum[0] += p.momentum
+
+		# case: top and bottom edges
+		if (ball.top[0] < 0 or ball.top[0] > 32):
+			ball.momentum[0] *= -1
+
 
 		# decay / clip momentum
-		ball.momentum = np.clip(ball.momentum, -2, 2) 
+		ball.momentum = np.clip(ball.momentum, -3, 3) 
 
 
 		# position update
@@ -72,53 +106,104 @@ class PongState(object):
 
 		# check for out-of-bounds
 		if (ball.top[1] < 0 or ball.top[1] > 31): 
-			ball = self.Ball(top=(15,15), size=2)
+			ball.__init__()
 
 			# score logic would go here
 
 	class Ball(object):
 		
-		def __init__(self, top=(15,15), size=2):
-			self.top = top
+		def __init__(self, top=(15,15), size=2, rnd=np.random.RandomState()):
+			self.rnd = rnd
+			self.top = np.array(top)
 			self.size=size
-			self.momentum = (1,1)
+			m_x = self.rnd.choice([-1,1])
+			m_y = self.rnd.choice([-1,1])
+			# m_x, m_y = -1,1
+			self.momentum = np.array([m_x,m_y])
 
 	class Paddle(object): 
 		
-		def __init__(self, side='left', size=5): 
-			self.top = (13,2)
+		def __init__(self, side='left', size=5, rnd=np.random.RandomState()): 
+			self.rnd = rnd
+			height = self.rnd.choice(24)
+			self.top = np.array((height+4,2))
 			if side == 'right': 
-				self.top = (13, 29)
+				self.top = np.array((height+4, 29))
 
 			self.size = size
 			self.momentum = 0 
 
 
+class BallChaseAI(object): 
 
+	def __init__(self):
+		pass
+
+	def getMove(self, ball, paddle): 
+		input = 0
+		if (ball.top[0] + ball.size/2) > (paddle.top[0] + paddle.size/2): 
+			input = 1
+
+		if (ball.top[0] + ball.size/2) < (paddle.top[0] + paddle.size/2): 
+			input = -1
+
+
+		return input
 
 
 
 def main():
-	input1 = None
-	input2 = None
+	player1 = BallChaseAI()
+	player2 = BallChaseAI()
 	pong = PongState()
 	keep_going = True
 
 	plt.axis([0,32,0,32])
-	#plt.ion()
+	plt.ion()
 	#plt.show()
 
-	while(keep_going): 
+
+	i=0
+	while(i < 50): 
+		input1 = player1.getMove(pong.ball, pong.paddle_1)
+		input2 = player2.getMove(pong.ball, pong.paddle_2)
 		pong.update(input1, input2)
 		screen = pong.draw()
 		plt.imshow(screen)
 		plt.draw()
 		plt.show()
-		time.sleep(5.0)
+		plt.pause(0.01)
+		i+=1
 		#keep_going = False
 		#print np.where(screen != 0)
 
+def gif_mode():
+	from PIL import Image, ImageSequence
+	player1 = BallChaseAI()
+	player2 = BallChaseAI()
+	pong = PongState()
+	screen = []
+
+
+	i=0
+	while(i < 50): 
+		input1 = player1.getMove(pong.ball, pong.paddle_1)
+		input2 = player2.getMove(pong.ball, pong.paddle_2)
+		pong.update(input1, input2)
+		screen = pong.draw()
+		i+=1
+		#keep_going = False
+		#print np.where(screen != 0)
+		# screen = screen[:,:]
+		screen = screen * 255
+		im = Image.fromarray(screen, mode='L')
+		# im.show()
+		im.save('frame' + str(i) + '.gif')
+
+	# ims = [Image.fromarray(s) for s in screen]
+	# ims.save('ani.gif', save_all=True)
+
 
 if __name__ == "__main__":
-	main()
+	gif_mode()
 
